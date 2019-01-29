8aa18db426ff1a767bc4222d5cbfd5a533715241
Samarin.E.Home <samarin_ey@hotmail.com>
2019-01-29 17:25:55 +0900

    #FTR add base cat function
    Добавлена базовая функция получения фото с сайта flickr

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Samar
+ * Date: 29.01.2019
+ * Time: 16:25
+ */?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Поймай кота</title>
+    <script type="text/javascript" src="/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="/photoGrabber.js"></script>
+</head>
+<body>
+<div align="center">
+    <button style="margin-bottom: 10px;" onclick="getPhoto()">Получить кота</button>
+    <div id="output"></div>
+</div>
+</body>
+</html>
